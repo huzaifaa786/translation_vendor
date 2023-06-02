1ee6736dc12c90993af721d9b063fc678c7e15dd
Adil Awan <tritechuzaifa@gmail.com>
2023-06-02 16:17:34 +0500

    Adil, 02/06/2023

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,7 +1,6 @@
 class User {
   int? id;
   String? username;
-  String? apiToken;
   String? number;
   String? email;
   String? profilePic;
@@ -10,7 +9,6 @@ class User {
     id = user['id'];
     username = user['username'];
     number = user['phone'];
-    apiToken = user['api_token'] ?? '';
     email = user['email'];
     profilePic = user['profilepic'] ?? '';
   }
